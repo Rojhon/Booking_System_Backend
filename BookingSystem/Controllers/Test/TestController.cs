dc8d1c32bcd712c56174fc4679e54a8242eaa91d
GiannPogi <gcmediavillo@gmail.com>
2022-06-15 15:01:27 +0800

    DAO Completed

--- BookingSystem/Controllers/Test/TestController.cs
+++ BookingSystem/Controllers/Test/TestController.cs
@@ -6,7 +6,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using BookingSystem.Models.Test;
-using BookingSystem.DAO.Test;
+using BookingSystem.Data.Test;
 
 namespace BookingSystem.Controllers.Test
 {
@@ -24,26 +24,38 @@ namespace BookingSystem.Controllers.Test
             return body;
         }
 
+        [Route("api/test/delete-test/{id}")]
+        [HttpPost]
+        public string DeleteTest(int id)
+        {
+            //System.Diagnostics.Debug.WriteLine(body);
+            testDAO.Delete(id);
+            return "Test deleted!!!";
+        }
+
+        [Route("api/test/update-test")]
+        [HttpPost]
+        public TestModel UpdateTest([FromBody] TestModel body)
+        {
+            //System.Diagnostics.Debug.WriteLine(body);
+            testDAO.Update(body);
+            return body;
+        }
+
+        [Route("api/test/details-test/{id}")]
+        [HttpGet]
+        public TestModel DetailsTest(int id)
+        {
+            //System.Diagnostics.Debug.WriteLine(body);
+            TestModel testModel = testDAO.Details(id);
+            return testModel;
+        }
+
         [Route("api/test/get-all-test")]
         [HttpGet]
         public List<TestModel> GetAllTest()
         {
             List<TestModel> testModel = testDAO.GetAll();
-            return testModel;
-        }
-
-        [Route("api/test/get-test")]
-        [HttpGet]
-        public List<TestModel> GetTest()
-        {
-            List<TestModel> testModel = new List<TestModel>()
-            {
-                new TestModel(){ Id = 1, Name="Rojhon" },
-                new TestModel(){ Id = 2, Name="Giann" },
-                new TestModel(){ Id = 3, Name="Dek" },
-                new TestModel(){ Id = 4, Name="Von" }
-            };
-
             return testModel;
         }
     }
--- BookingSystem/Data/Test/TestDAO.cs
+++ BookingSystem/Data/Test/TestDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
 using BookingSystem.Models.Test;
+using System.Data;
 
 namespace BookingSystem.Data.Test
 {
@@ -23,6 +24,59 @@ namespace BookingSystem.Data.Test
                 command.Parameters.AddWithValue("@Name", testModel.Name);
                 command.ExecuteNonQuery();
             }
+        }
+
+        public void Delete(int Id)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Delete from Test Where Id=@Id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Id", Id);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(TestModel testModel)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Update Test Set Name=@Name Where Id=@Id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Id", testModel.Id);
+                cmd.Parameters.AddWithValue("@Name", testModel.Name);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public TestModel Details(int Id)
+        {
+            TestModel returnList = new TestModel();
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "Select * from Test Where Id=@Id";
+                SqlCommand command = new SqlCommand(query, con);
+                command.Parameters.AddWithValue("@Id", Id);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        TestModel testModel = new TestModel();
+                        testModel.Id = reader.GetInt32(0);
+                        testModel.Name = reader.GetString(1);
+
+                        returnList = testModel;
+                    }
+                }
+            }
+
+            return returnList;
         }
 
         public List<TestModel> GetAll()
