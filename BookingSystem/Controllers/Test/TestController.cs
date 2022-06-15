fbe5283f0db1ea974f8df3ac83ca2f5ee6029b0e
Rojhon <rojhonbuenaventura5@gmail.com>
2022-06-15 09:50:52 +0800

    basic dao

--- BookingSystem/Controllers/Test/TestController.cs
+++ BookingSystem/Controllers/Test/TestController.cs
@@ -13,7 +13,38 @@ namespace BookingSystem.Controllers.Test
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class TestController : ApiController
     {
-        public TestModel testModel = new TestModel();
         public TestDAO testDAO = new TestDAO();
+
+        [Route("api/test/create-test")]
+        [HttpPost]
+        public TestModel CreateTest([FromBody] TestModel body)
+        {
+            //System.Diagnostics.Debug.WriteLine(body);
+            testDAO.Create(body);
+            return body;
+        }
+
+        [Route("api/test/get-all-test")]
+        [HttpGet]
+        public List<TestModel> GetAllTest()
+        {
+            List<TestModel> testModel = testDAO.GetAll();
+            return testModel;
+        }
+
+        [Route("api/test/get-test")]
+        [HttpGet]
+        public List<TestModel> GetTest()
+        {
+            List<TestModel> testModel = new List<TestModel>()
+            {
+                new TestModel(){ Id = 1, Name="Rojhon" },
+                new TestModel(){ Id = 2, Name="Giann" },
+                new TestModel(){ Id = 3, Name="Dek" },
+                new TestModel(){ Id = 4, Name="Von" }
+            };
+
+            return testModel;
+        }
     }
 }
