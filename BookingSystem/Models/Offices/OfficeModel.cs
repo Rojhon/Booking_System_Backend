c341273fd891f164e6991b08b3eb04433ff00163
GiannPogi <gcmediavillo@gmail.com>
2022-06-15 14:05:06 +0800

    Models added

---
+++ BookingSystem/Models/Offices/OfficeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingSystem.Models
+{
+    public class OfficeModel
+    {
+        public int OfficeId { get; set; }
+        public string OfficeName { get; set; }
+        public string OfficeDescription { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
