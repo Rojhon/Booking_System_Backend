91eaf95b61e5fe8c1920bdfe227ebad1b01c59db
Kyle Reneg Aduan <kylereneg1234@gmail.com>
2022-06-23 18:12:00 +0800

    Updated Register

--- BookingSystem/Controllers/User/RegisterController.cs
+++ BookingSystem/Controllers/User/RegisterController.cs
@@ -7,7 +7,38 @@ using System.Web.Http;
 
 namespace BookingSystem.Controllers.User
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class RegisterController : ApiController
     {
+        public RegisterDAO registerDAO = new RegisterDAO();
+
+        [Route("api/users/create")]
+        [HttpPost]
+        public string CreateUser([FromBody]UserModel body)
+        {
+            return registerDAO.InsertOne(body);
+        }
+
+        [Route("api/users/get/{Id}")]
+        [HttpGet]
+        public List<UserModel> GetUser(string Id)
+        {
+            return registerDAO.FindOne(Id);
+        }
+
+        [Route("api/users/update")]
+        [HttpPost]
+        public string UpdateUser([FromBody] UserModel body)
+        {
+            return registerDAO.UpdateOne(body);
+        }
+
+        [Route("api/users/delete/{Id}")]
+        [HttpDelete]
+        public string DeleteUser(string Id)
+        {
+            return registerDAO.DeleteOne(Id);
+        }
+
     }
 }
--- BookingSystem/Data/User/RegisterDAO.cs
+++ BookingSystem/Data/User/RegisterDAO.cs
@@ -7,5 +7,67 @@ namespace BookingSystem.Data.User
 {
     public class RegisterDAO
     {
+        private string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BookingSystemDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public string InsertOne(UserModel UserModel)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sqlQuery = "INSERT INTO Users (UserNumber, FirstName, LastName, Position, Email, Password, CreatedAt) Values(@UserNumber, @FirstName, @LastName, @Position, @Email, @Password, GETDATE())";
+
+                    SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+                    command.Parameters.AddWithValue("@UserNumber", UserModel.UserNumber);
+                    command.Parameters.AddWithValue("@FirstName", UserModel.FirstName);
+                    command.Parameters.AddWithValue("@LastName", UserModel.LastName);
+                    command.Parameters.AddWithValue("@Position", UserModel.Position);
+                    command.Parameters.AddWithValue("@Email", UserModel.Email);
+                    command.Parameters.AddWithValue("@Password", UserModel.Password);
+                    command.Parameters.AddWithValue("@Password", UserModel.Password);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+                    return "Success";
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                return "Error";
+            }
+        }
+        public string UpdateOne(UserModel UserModel)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "Update Requests Set UserNumber=@UserNumber, FirstName=@FirstName, LastName=@LastName, Position=@Position, Email=@Email, Password=@Password UpdatedAt=GETDATE() Where UserNumber=@UserNumber";
+
+                    SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+                    command.Parameters.AddWithValue("@UserNumber", UserModel.UserNumber);
+                    command.Parameters.AddWithValue("@FirstName", UserModel.FirstName);
+                    command.Parameters.AddWithValue("@LastName", UserModel.LastName);
+                    command.Parameters.AddWithValue("@Position", UserModel.Position);
+                    command.Parameters.AddWithValue("@Email", UserModel.Email);
+                    command.Parameters.AddWithValue("@Password", UserModel.Password);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+
+                    return "Updated";
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e);
+                return "Error";
+            }
+        }
+
     }
 }
